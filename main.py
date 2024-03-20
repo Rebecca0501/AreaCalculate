946b0b9f29f11fd2f3603c39e413ff341b2a4217
dc <rebecca.s.home51@gmail.com>
2024-03-20 15:49:51 +0800

    cleaning code

--- main.py
+++ main.py
@@ -68,6 +68,42 @@ class detA_sum():
             sum_detA += s
         return sum_detA
 
+def getFloorBorder(msp, LayerName:str, FloorDataSheet):
+    for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format(LayerName)):
+        LWP_point = LWPOLYLINE.get_points()
+        p1 = CPoint(LWP_point[0][0],LWP_point[0][1])
+        p2 = CPoint(LWP_point[1][0],LWP_point[1][1])
+        p3 = CPoint(LWP_point[2][0],LWP_point[2][1])
+        p4 = CPoint(LWP_point[3][0],LWP_point[3][1])
+        select_area = CSqure(CLine(p1,p2),CLine(p4,p3))
+        
+        x_min = select_area.x_min()
+        x_max = select_area.x_max()
+        y_min = select_area.y_min()
+        y_max = select_area.y_max()
+        new_floor = floor_imformation(x_min,x_max,y_min,y_max)
+        FloorDataSheet.append(new_floor)
+
+def getFloorName(msp, LayerName:str, FloorDataSheet):
+    for TEXT in msp.query("TEXT[layer=='{}']".format(LayerName)):
+        TextCoordinate = TEXT.dxf.insert
+        TextCoordiPoint = CPoint(TextCoordinate[0],TextCoordinate[1])
+        text_py = TEXT.dxf.text
+        for idx, FloorInfo in enumerate(FloorDataSheet):
+            if FloorInfo.x_min < TextCoordiPoint.x and TextCoordiPoint.x < FloorInfo.x_max and FloorInfo.y_min < TextCoordiPoint.y and TextCoordiPoint.y < FloorInfo.y_max:
+                FloorDataSheet[idx].name = text_py
+                break
+
+    for MTEXT in msp.query("MTEXT[layer=='{}']".format(LayerName)):
+        TextCoordinate = MTEXT.dxf.insert
+        TextCoordiPoint = CPoint(TextCoordinate[0],TextCoordinate[1])
+        text_py = MTEXT.dxf.text
+        for idx, FloorInfo in enumerate(FloorDataSheet):
+            if FloorInfo.x_min < TextCoordiPoint.x and TextCoordiPoint.x < FloorInfo.x_max and FloorInfo.y_min < TextCoordiPoint.y and TextCoordiPoint.y < FloorInfo.y_max:
+                FloorDataSheet[idx].name = text_py
+                break
+
+
 if __name__=="__main__":
 
     #Step1. Get CAD file path
@@ -76,7 +112,7 @@ if __name__=="__main__":
     pathInputFilePath=pathlib.Path(os.getcwd())
     pathInputFilePath=pathInputFilePath.joinpath(str_CAD_file_name)
     input_file_path = str(pathInputFilePath)
-    print(input_file_path)
+    #print(input_file_path)
 
 
     #Step2. Open CAD file and model space
@@ -94,115 +130,84 @@ if __name__=="__main__":
     # "PointNumber" for PointNumber label, the color is white
     #doc.layers.add(name="PointNumber")
 
-    #Step4. record imformation of select area
-    floor_object = []
-
+    #Step4. create floor data sheet record imformation of select area
+    FloorDataSheet = []
     
+    getFloorBorder(msp, "SelectArea", FloorDataSheet)
 
-    for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("SelectArea")):
-        LWP_point = LWPOLYLINE.get_points()
-        p1 = CPoint(LWP_point[0][0],LWP_point[0][1])
-        p2 = CPoint(LWP_point[1][0],LWP_point[1][1])
-        p3 = CPoint(LWP_point[2][0],LWP_point[2][1])
-        p4 = CPoint(LWP_point[3][0],LWP_point[3][1])
-        select_area = CSqure(CLine(p1,p2),CLine(p4,p3))
-        
-        x_min = select_area.x_min()
-        x_max = select_area.x_max()
-        y_min = select_area.y_min()
-        y_max = select_area.y_max()
-        new_floor = floor_imformation(x_min,x_max,y_min,y_max)
-        floor_object.append(new_floor)
-
-    for TEXT in msp.query("TEXT[layer=='{}']".format("floor_name")):
-        get_placement = TEXT.dxf.insert
-        point = CPoint(get_placement[0],get_placement[1])
-        text_py = TEXT.dxf.text
-        for idx, f in enumerate(floor_object):
-            if f.x_min < point.x and point.x < f.x_max and f.y_min < point.y and point.y < f.y_max:
-                floor_object[idx].name = text_py
-                break
-
-    for MTEXT in msp.query("MTEXT[layer=='{}']".format("floor_name")):
-        get_placement = MTEXT.dxf.insert
-        point = CPoint(get_placement[0],get_placement[1])
-        text_py = MTEXT.dxf.text
-        for idx, f in enumerate(floor_object):
-            if f.x_min < point.x and point.x < f.x_max and f.y_min < point.y and point.y < f.y_max:
-                floor_object[idx].name = text_py
-                break
+    getFloorName(msp, "floor_name", FloorDataSheet)
     
 
     #Step5. calculate each function area
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("41-樓地板面積")):
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_floor += sum_detA
+                FloorDataSheet[idx].area_floor += sum_detA
                 break
 
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("42-梯廳面積")):
         
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_hall += sum_detA
+                FloorDataSheet[idx].area_hall += sum_detA
                 break
     
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("43-管委會面積")):
         
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_committee += sum_detA
+                FloorDataSheet[idx].area_committee += sum_detA
                 break
 
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("44-機電面積")):
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_electromechanical += sum_detA
+                FloorDataSheet[idx].area_electromechanical += sum_detA
                 break
 
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("45-車道面積")):
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_carramp += sum_detA
+                FloorDataSheet[idx].area_carramp += sum_detA
                 break
 
     for LWPOLYLINE in msp.query("LWPOLYLINE[layer=='{}']".format("46-陽台面積")):
         points = point_list(LWPOLYLINE.get_points()).result()
         IntPointNum = len(points)
-        for idx, f in enumerate(floor_object):
+        for idx, f in enumerate(FloorDataSheet):
             if f.x_min < points[0].x and points[0].x < f.x_max and f.y_min < points[0].y and points[0].y < f.y_max:
                 #****************Be careful unit conversion**********
                 sum_detA = abs(detA_sum(IntPointNum,points).result())*0.0001
                 #****************Be careful unit conversion**********
-                floor_object[idx].area_balcony += sum_detA
+                FloorDataSheet[idx].area_balcony += sum_detA
                 break
 
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
 
         f.area_floorRemoveCar = f.area_floor - f.area_carramp
 
@@ -355,77 +360,77 @@ if __name__=="__main__":
     s1 = wb.active 
 
     s1['A1'].value = '樓層'
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"A{idx+2}")
-        s1[cell].value = floor_object[idx].name
+        s1[cell].value = FloorDataSheet[idx].name
 
     s1['B1'].value = '樓地版面積'
     s1['B1'].fill = PatternFill(fill_type="solid", fgColor="FDE9D9")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"B{idx+2}")
-        s1[cell].value = floor_object[idx].area_floor
+        s1[cell].value = FloorDataSheet[idx].area_floor
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="FDE9D9")
 
     s1['C1'].value = '梯廳面積'
     s1["C1"].fill = PatternFill(fill_type="solid", fgColor="DAEEF3")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"C{idx+2}")
-        s1[cell].value = floor_object[idx].area_hall
+        s1[cell].value = FloorDataSheet[idx].area_hall
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="DAEEF3")
 
     s1['D1'].value = '機電面積'
     s1["D1"].fill = PatternFill(fill_type="solid", fgColor="E4DFEC")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"D{idx+2}")
-        s1[cell].value = floor_object[idx].area_electromechanical
+        s1[cell].value = FloorDataSheet[idx].area_electromechanical
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="E4DFEC")
 
     s1['E1'].value = '管委會面積'
     s1['E1'].fill = PatternFill(fill_type="solid", fgColor="EBF1DE")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"E{idx+2}")
-        s1[cell].value = floor_object[idx].area_committee
+        s1[cell].value = FloorDataSheet[idx].area_committee
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="EBF1DE")
 
     s1['F1'].value = '陽台面積'
     s1['F1'].fill = PatternFill(fill_type="solid", fgColor="F2DCDB")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"F{idx+2}")
-        s1[cell].value = floor_object[idx].area_balcony
+        s1[cell].value = FloorDataSheet[idx].area_balcony
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="F2DCDB")
 
     s1['G1'].value = '室內停車位與車道面積'
     s1['G1'].fill = PatternFill(fill_type="solid", fgColor="F2F2F2")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"G{idx+2}")
-        s1[cell].value = floor_object[idx].area_carramp
+        s1[cell].value = FloorDataSheet[idx].area_carramp
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="F2F2F2")
 
     s1['H1'].value = '梯廳反計容積'
     s1['H1'].fill = PatternFill(fill_type="solid", fgColor="FCD5B4")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"H{idx+2}")
-        s1[cell].value = floor_object[idx].over_hall
+        s1[cell].value = FloorDataSheet[idx].over_hall
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="FCD5B4")
     
     s1['I1'].value = '陽台反計容積'
     s1['I1'].fill = PatternFill(fill_type="solid", fgColor="E6B8B7")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"I{idx+2}")
-        s1[cell].value = floor_object[idx].over_balcony
+        s1[cell].value = FloorDataSheet[idx].over_balcony
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="E6B8B7")
 
     s1['J1'].value = '梯廳與陽台反計容積'
     s1['J1'].fill = PatternFill(fill_type="solid", fgColor="D8E4BC")
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"J{idx+2}")
-        s1[cell].value = floor_object[idx].over_hallBalcony
+        s1[cell].value = FloorDataSheet[idx].over_hallBalcony
         s1[cell].fill = PatternFill(fill_type="solid", fgColor="D8E4BC")
 
     s1['K1'].value = '容積樓地板面積'
-    for idx, f in enumerate(floor_object):
+    for idx, f in enumerate(FloorDataSheet):
         cell = str(f"K{idx+2}")
-        s1[cell].value = floor_object[idx].area_volumn
+        s1[cell].value = FloorDataSheet[idx].area_volumn
         
 
     wb.save('面積計算表.xlsx')
